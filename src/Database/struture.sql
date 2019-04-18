b80eb611267782e91f7c42b417a5df9805320afa
Quentin B <quentin.bisiaux@gmail.com>
2019-04-19 01:08:45 +0200

    new database

--- src/Database/struture.sql
+++ src/Database/struture.sql
@@ -1,17 +1,19 @@
-CREATE DATABASE `memory` /*!40100 DEFAULT CHARACTER SET latin1 */;
+/* NE PAS COPIER CETTE LIGNE SI C'EST LA PREMIERE FOIS QUE VOUS INITIALISER VOTRE BDD */
+DROP DATABASE `hackaton`
+
+CREATE DATABASE `hackaton` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE hackaton;
 
 CREATE TABLE `score` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `score` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `character_id` int(11) DEFAULT NULL,
-  `party_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_score_user_idx` (`user_id`),
-  KEY `fk_score_party_idx` (`party_id`),
   KEY `fk_score_character_idx` (`character_id`),
   CONSTRAINT `fk_score_character` FOREIGN KEY (`character_id`) REFERENCES `character` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_score_party` FOREIGN KEY (`party_id`) REFERENCES `party` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_score_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -20,13 +22,10 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(128) NOT NULL,
+  `gold` INT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `memory`.`party` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `number_cards` INT NULL,
-  PRIMARY KEY (`id`));
 
 CREATE TABLE `inventory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -47,6 +46,43 @@ CREATE TABLE `character` (
 CREATE TABLE `userCharacter` (
   `user_id` int(11) NOT NULL,
   `character_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`character_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+INSERT INTO `user` (username, password)
+VALUES  ("Julien", "Julien"),
+        ("Nathalie", "Nathalie"),
+        ("Fouc", "Fouc"),
+        ("Quentin", "Quentin");
+
+INSERT INTO `character`(API_id)
+VALUES ("5cac51240d488f0da6151c5e"),
+       ("5cac51240d488f0da6151c4c"),
+       ("5cac51240d488f0da6151c3d"),
+       ("5cac51240d488f0da6151c34");
+
+INSERT INTO `userCharacter` (user_id, character_id)
+VALUES (1, 1),
+       (1, 3),
+       (2, 2),
+       (2, 4),
+       (3, 3),
+       (4, 4);
+
+INSERT INTO score (score, user_id, character_id)
+VALUES (2580, 1, 1 ),
+       (3129, 2, 2 ),
+       (2360, 3, 3 ),
+       (1555, 4, 4 );
+
+INSERT INTO `inventory` (egg_Api, user_id)
+VALUES ("5cac51240d488f0da6151bd6" , 1),
+       ("5cac51240d488f0da6151bd1" , 1),
+       ("5cac51240d488f0da6151bce" , 1),
+       ("5cac51240d488f0da6151c06" , 2),
+       ("5cac51240d488f0da6151bef" , 2),
+       ("5cac51240d488f0da6151c12" , 3),
+       ("5cac51240d488f0da6151bf8" , 4),
+       ("5cac51240d488f0da6151c22" , 4),
+       ("5cac51240d488f0da6151bf7" , 3),
+       ("5cac51240d488f0da6151c1d" , 4);
