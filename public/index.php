a4842344ad5405ae828569769645ef51a995e1bf
DocteurXXX <juliano070177@gmail.com>
2019-04-19 10:48:29 +0200

    modif

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@
 session_start();
 
 
-session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/config.php';
